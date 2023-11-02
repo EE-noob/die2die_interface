46af9fc6e633ee4c2e19dd2fa516ec3812acf76e
Core_kingdom <2972880695@qq.com>
2023-11-02 12:47:00 +0800

    0mine

---
+++ v1.0/rev1/verdiLog/verdi.cmd
@@ -0,0 +1,16 @@
+debImport "./aib_lib" "./how2use" "./maib_rtl" "./ndsimslv" "./novas.conf" \
+          "./novas.rc" "./README.txt" "./rtl" "./verdiLog"
+nsMsgSelect -range {0 0-3}
+nsMsgSelect -range {0 6-6}
+nsMsgSelect -range {0 5-5}
+nsMsgSelect -range {0 2-2}
+nsMsgSelect -range {0 3-3}
+nsMsgSelect -range {0 1-1}
+nsMsgSelect -range {0 0-0}
+nsMsgSelect -range {0 2-2}
+nsMsgSelect -range {0 3-3}
+nsMsgSelect -range {0 4-4}
+nsMsgSelect -range {0 5-5}
+nsMsgSelect -range {0 6-7}
+nsMsgSelect -range {0 15-15}
+debExit
